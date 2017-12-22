ec515e55b0a778f22f6da21fb0a92237d22cdb37
Wish_Time <wish-time@outlook.com>
2017-12-22 15:16:47 +0800

    Initial commit

---
+++ skins/back_to_top.js
@@ -0,0 +1,30 @@
+﻿/*浮动窗口*/
+// (function(){
+// 	var n=400;
+// 	var obj=document.getElementById("AdLayer");
+// 	if(!obj){return false;}
+// 	var x=0;
+// 	var fe=$("#AdLayer");
+// 	window.onscroll=function(){
+// 		obj.style.top=(document.body.scrollTop||document.documentElement.scrollTop)+n+'px';
+// 		x=(document.body.scrollTop||document.documentElement.scrollTop)+n;
+// 		if(x<=460){fe.fadeOut().hide()}else{fe.fadeIn().show()};
+// 	};
+// 	window.onresize=function(){obj.style.top=(document.body.scrollTop||document.documentElement.scrollTop)+n+'px'};
+// })();
+
+$('#AdLayer').hide();
+	 $(function(){
+            $(window).scroll(function(){
+                if($(this).scrollTop()>1){//当window的scrolltop距离大于1时，go to top按钮淡出，反之淡入
+                    $("#AdLayer").fadeIn();
+                } else {
+                    $("#AdLayer").fadeOut();
+                }
+            });
+			
+        });
+	
+	$('#AdLayer a').click(function(){
+		$('html,body').animate({scrollTop:0},'slow');
+	});
---
+++ skins/mapsclick.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+	// 新疆
+	new PopupLayer({trigger:"#gd",popupBlk:"#blkgd",closeBtn:"#closegd",useOverlay:true});
+	new PopupLayer({trigger:"#hl",popupBlk:"#blkhl",closeBtn:"#closehl",useOverlay:true});
+	new PopupLayer({trigger:"#hj",popupBlk:"#blkhj",closeBtn:"#closehj",useOverlay:true});
+	new PopupLayer({trigger:"#zj",popupBlk:"#blkzj",closeBtn:"#closezj",useOverlay:true});
+	new PopupLayer({trigger:"#sh",popupBlk:"#blksh",closeBtn:"#closesh",useOverlay:true});
+	new PopupLayer({trigger:"#hun",popupBlk:"#blkhun",closeBtn:"#closehun",useOverlay:true});
+	new PopupLayer({trigger:"#hub",popupBlk:"#blkhub",closeBtn:"#closehub",useOverlay:true});
+	new PopupLayer({trigger:"#jx",popupBlk:"#blkjx",closeBtn:"#closejx",useOverlay:true});
+	new PopupLayer({trigger:"#ah",popupBlk:"#blkah",closeBtn:"#closeah",useOverlay:true});
+	new PopupLayer({trigger:"#hn",popupBlk:"#blkhn",closeBtn:"#closehn",useOverlay:true});
+	new PopupLayer({trigger:"#sd",popupBlk:"#blksd",closeBtn:"#closesd",useOverlay:true});
+	new PopupLayer({trigger:"#ln",popupBlk:"#blkln",closeBtn:"#closeln",useOverlay:true});
+	//new PopupLayer({trigger:"#jl",popupBlk:"#blkjl",closeBtn:"#closejl",useOverlay:true});
+	new PopupLayer({trigger:"#nx",popupBlk:"#blknx",closeBtn:"#closenx",useOverlay:true});
+	// new PopupLayer({trigger:"#hb",popupBlk:"#blkhb",closeBtn:"#closehb",useOverlay:true});
+	new PopupLayer({trigger:"#bj",popupBlk:"#blkbj",closeBtn:"#closebj",useOverlay:true});
+	new PopupLayer({trigger:"#tj",popupBlk:"#blktj",closeBtn:"#closetj",useOverlay:true});
+	new PopupLayer({trigger:"#sc",popupBlk:"#blksc",closeBtn:"#closesc",useOverlay:true});
+	new PopupLayer({trigger:"#gx",popupBlk:"#blkgx",closeBtn:"#closegx",useOverlay:true});
+	new PopupLayer({trigger:"#cq",popupBlk:"#blkcq",closeBtn:"#closecq",useOverlay:true});
+	new PopupLayer({trigger:"#yn",popupBlk:"#blkyn",closeBtn:"#closeyn",useOverlay:true});
+	new PopupLayer({trigger:"#gz",popupBlk:"#blkgz",closeBtn:"#closegz",useOverlay:true});
+	new PopupLayer({trigger:"#shx",popupBlk:"#blkshx",closeBtn:"#closeshx",useOverlay:true});
+	new PopupLayer({trigger:"#js",popupBlk:"#blkjs",closeBtn:"#closesjs",useOverlay:true});
+	// new PopupLayer({trigger:"#hl",popupBlk:"#blkxj",closeBtn:"#closexj",useOverlay:true});
+	// new PopupLayer({trigger:"#gx",popupBlk:"#blkxj",closeBtn:"#closexj",useOverlay:true});
+	// new PopupLayer({trigger:"#gz",popupBlk:"#blkxj",closeBtn:"#closexj",useOverlay:true});
+	// new PopupLayer({trigger:"#yn",popupBlk:"#blkxj",closeBtn:"#closexj",useOverlay:true});
+	// new PopupLayer({trigger:"#sc",popupBlk:"#blkxj",closeBtn:"#closexj",useOverlay:true});
+	// new PopupLayer({trigger:"#cq",popupBlk:"#blkxj",closeBtn:"#closexj",useOverlay:true});
+	// new PopupLayer({trigger:"#gx",popupBlk:"#blkxj",closeBtn:"#closexj",useOverlay:true});
+	// new PopupLayer({trigger:"#hun",popupBlk:"#blkxj",closeBtn:"#closexj",useOverlay:true});
+	// new PopupLayer({trigger:"#hub",popupBlk:"#blkxj",closeBtn:"#closexj",useOverlay:true});
+	// new PopupLayer({trigger:"#tw",popupBlk:"#blkxj",closeBtn:"#closexj",useOverlay:true});
+	// new PopupLayer({trigger:"#xg",popupBlk:"#blkxj",closeBtn:"#closexj",useOverlay:true});
+
+})
+		
